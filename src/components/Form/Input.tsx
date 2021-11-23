32aa4c79cda82b9efe971ef5b43a38ec853144de
Saull Brandão <saull@outlook.com>
2021-11-23 08:49:55 -0300

    feat: create sign in page

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,29 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react'
+
+type InputProps = ChakraInputProps & {
+  name: string
+  label?: string
+}
+
+export const Input = ({ name, label, ...restProps }: InputProps) => {
+  return (
+    <FormControl>
+      {label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{ bg: 'gray.900' }}
+        size="lg"
+        {...restProps}
+      />
+    </FormControl>
+  )
+}
