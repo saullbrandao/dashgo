6a32403f9c2f6a8d10d7179b4c8dca505efeed60
Saull Brandão <saull@outlook.com>
2021-11-24 10:06:36 -0300

    feat: change the padding on smaller devices

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -69,7 +69,7 @@ export default function Dashboard() {
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
